721f1e4aa114079e9e36fa015933689a6b653c89
ClassicSmej <jamesw.white96@gmail.com>
2023-06-01 16:39:24 +0100

    remove update state

--- tube-trackr/src/components/Update.jsx
+++ tube-trackr/src/components/Update.jsx
@@ -1,17 +1,4 @@
-import React, { useEffect, useState } from 'react'
-
 function Update({ update, isActive }) {
-
-    const [showLink, setShowLink] = useState(false)
-
-    // useEffect(
-    //     function getLink() {
-    //         if (update.includes("https")) {
-    //             console.log(true)
-    //             // setShowLink(true)
-    //         }
-    //     },[]
-    // );
 
     return (
         <>
