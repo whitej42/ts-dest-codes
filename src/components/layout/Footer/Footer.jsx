9b57b4b70a6b995b9e2a57cec22330ed641a0cd5
James White <44773230+whitej42@users.noreply.github.com>
2024-10-28 17:35:00 +0000

    Resolve problem components (#16)
    * improved file structure and updated layout
    
    * added headcode generator and additional pages
    
    * improved headcode generator and css tweaks
    
    * save custom headcodes feature and local storage enabled
    
    * Rename Navbar component
    
    * rename files temp to solve merge conflict
    
    * resolve navbar conflict
    
    * add missing index css
    
    * udpate grid links
    
    * tmp rename navbar and footer dirs
    
    * rename problem components

---
+++ src/components/layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <label>All Rights Reserved © 2023</label>
+    </div>
+  )
+}
+
+export default Footer
