87648aaabd0389fbf12fe5ca4f3bfeddbb2216fb
James White <44773230+whitej42@users.noreply.github.com>
2025-04-10 11:40:29 +0100

    Update Footer.jsx

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <div className='footer'>
-      <label>All Rights Reserved © 2023</label>
+      <label>All Rights Reserved © 2025</label>
     </div>
   )
 }
